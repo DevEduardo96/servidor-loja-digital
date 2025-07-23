f1ffdf43b1bc4e1317f092e9c2d55c55168ac9a2
Seu Nome <eduardoaraujo_96@outlook.com>
2025-07-23 09:11:02 -0500

    corrigindo problemas de importação

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const { MercadoPagoConfig, Payment } = require("mercadopago");
-const produtos = require("./Produtos");
+const { produtos } = require("./Produtos");
 
 const app = express();
 app.use(cors());
